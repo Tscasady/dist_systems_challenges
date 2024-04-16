83769b76b1f41bc141ca7152e43132e8b304eab8
tsc <tscasady@gmail.com>
2024-04-16 15:54:16 -0500

    refactor echo messages.

--- src/bin/echo.rs
+++ src/bin/echo.rs
@@ -1,26 +1,9 @@
-use anyhow::Context;
-use dist_systems_challenge::{service, InitPayload, Message, MsgBody, Node, NodeBuilder, Payload};
+use dist_systems_challenge::{service, InitPayload, Message, Node, Payload};
 use serde::{Deserialize, Serialize};
-use std::io::{StdoutLock, Write};
-
-struct EchoBuilder;
-
-impl NodeBuilder<EchoNode> for EchoBuilder {
-    fn build(self, msg: Message<InitPayload>, output: &mut StdoutLock) -> anyhow::Result<EchoNode> {
-        let reply = Message {
-            src: msg.dest,
-            dest: msg.src,
-            body: MsgBody {
-                msg_id: Some(0),
-                in_reply_to: msg.body.msg_id,
-                payload: InitPayload::InitOk,
-            },
-        };
-        serde_json::to_writer(&mut *output, &reply).context("serialize response to echo msg")?;
-        output.write_all(b"\n").context("add newline")?;
-        Ok(EchoNode { id: 1 })
-    }
-}
+use std::{
+    io::StdoutLock,
+    sync::mpsc::Sender,
+};
 
 struct EchoNode {
     id: usize,
@@ -28,25 +11,24 @@ struct EchoNode {
 
 impl Node for EchoNode {
     type Payload = EchoPayload;
+
+    fn new(
+        msg: Message<InitPayload>,
+        output: &mut StdoutLock,
+        _tx: Sender<Message<EchoPayload>>,
+    ) -> anyhow::Result<EchoNode> {
+        msg.into_reply(Some(0), InitPayload::InitOk).write(&mut *output)?;
+        Ok(EchoNode { id: 1 })
+    }
+
     fn reply(
         &mut self,
-        input: Message<EchoPayload>,
+        mut input: Message<EchoPayload>,
         output: &mut StdoutLock,
     ) -> anyhow::Result<()> {
-        match input.body.payload {
+        match std::mem::replace(&mut input.body.payload, EchoPayload::Empty) {
             EchoPayload::Echo { echo } => {
-                let reply = Message {
-                    src: input.dest,
-                    dest: input.src,
-                    body: MsgBody {
-                        msg_id: Some(self.id),
-                        in_reply_to: input.body.msg_id,
-                        payload: EchoPayload::EchoOk { echo },
-                    },
-                };
-                serde_json::to_writer(&mut *output, &reply)
-                    .context("serialize response to echo msg")?;
-                output.write_all(b"\n").context("add newline")?;
+                input.into_reply(Some(self.id), EchoPayload::EchoOk { echo }).write(&mut *output)?;
                 self.id += 1;
             }
             _ => {}
@@ -61,11 +43,11 @@ impl Node for EchoNode {
 enum EchoPayload {
     Echo { echo: String },
     EchoOk { echo: String },
+    Empty
 }
 
 impl Payload for EchoPayload {}
 
 fn main() -> anyhow::Result<()> {
-    service(EchoBuilder {})
+    service::<EchoNode>()
 }
-
